28ae429ca3fd4691049316d7544f01b3b978ca7d
fernandes <fernandesdenzo223@gmail.com>
2025-02-20 18:21:56 -0300

    Navbar e fundo

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+particlesJS('particles-js', {
+    particles: {
+      number: { value: 100 },
+      color: { value: "#ffffff" },
+      shape: { type: "circle" },
+      opacity: { value: 0.7 },
+      size: { value: 2 },
+      move: { speed: 1 }
+    }
+  });
+  
