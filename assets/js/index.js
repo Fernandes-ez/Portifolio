aa971663ba2d7f24b9d7e470552ab85758210d4b
fernandes <fernandesdenzo223@gmail.com>
2025-02-24 15:17:09 -0300

    Finalização

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,19 @@
 particlesJS('particles-js', {
-    particles: {
-      number: { value: 100 },
-      color: { value: "#ffffff" },
-      shape: { type: "circle" },
-      opacity: { value: 0.7 },
-      size: { value: 2 },
-      move: { speed: 1 }
-    }
-  });
-  
+  particles: {
+    number: { value: 100 },
+    color: { value: "#ffffff" },
+    shape: { type: "circle" },
+    opacity: { value: 0.7 },
+    size: { value: 2 },
+    move: { speed: 1 }
+  }
+});
+
+// ScrollReveal configuration
+ScrollReveal().reveal('.inicioTitulo', { delay: 200 });
+ScrollReveal().reveal('.inicioSubtitulo', { delay: 400 });
+ScrollReveal().reveal('#curriculoBtn', { delay: 600 });
+ScrollReveal().reveal('.sobreMim', { delay: 800 });
+ScrollReveal().reveal('.habilidades', { delay: 1000 });
+ScrollReveal().reveal('.projetos', { delay: 1200 });
+ScrollReveal().reveal('.contato', { delay: 1400 });
